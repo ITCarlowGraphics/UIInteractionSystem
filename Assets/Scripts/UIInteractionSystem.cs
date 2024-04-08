4fd0b134e9a8cb5ca1b2dd274b85d6cf238b5157
haotai-xiong <c00265675@itcarlow.ie>
2024-04-08 09:50:21 +0100

    #1 Create Buttons

--- Assets/Scripts/UIInteractionSystem.cs
+++ Assets/Scripts/UIInteractionSystem.cs
@@ -1,47 +1,71 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SceneManagement;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class UIInteractionSystem : MonoBehaviour
 {
+    // gameobject attributes
+    Canvas canvas;
+    // button attributes
+    public bool createButtonOrNot;
+    public int buttonNumbers;
+
     // Start is called before the first frame update
     void Start()
     {
-        Canvas canvas = GetComponent<Canvas>();
-        if (canvas == null)
+        if (!TryGetComponent(out canvas))
         {
-            Debug.LogError("No Canvas found in the scene. Please add a Canvas to use UI elements.");
+            Debug.LogError("No Canvas found in the scene. Please add a Canvas in order to use UI elements.");
             return;
         }
 
-        GameObject buttonObj = new GameObject("MyButton");
-        buttonObj.transform.SetParent(this.transform, false);
-        RectTransform rectTransform = buttonObj.AddComponent<RectTransform>();
-        rectTransform.sizeDelta = new Vector2(160, 30);
-
-        Button button = buttonObj.AddComponent<Button>();
-        Image image = buttonObj.AddComponent<Image>();
-
-        buttonObj.transform.SetParent(canvas.transform, false);
-        GameObject textObj = new GameObject("ButtonText");
-        textObj.transform.SetParent(buttonObj.transform, false);
-
-        Text text = textObj.AddComponent<Text>();
-        text.text = "Click Me!";
-        text.font = Resources.GetBuiltinResource<Font>("LegacyRuntime.ttf");
-        text.color = Color.black;
-        text.alignment = TextAnchor.MiddleCenter;
-
-        RectTransform textRectTransform = textObj.GetComponent<RectTransform>();
-        textRectTransform.sizeDelta = new Vector2(160, 30);
-        button.onClick.AddListener(() => ButtonClicked());
+        if (createButtonOrNot)
+        {
+            CreateButtons();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void CreateButtons()
+    {
+        for (int i = 0; i < buttonNumbers; i++)
+        {
+            GameObject buttonObj = new("MyButton");
+            buttonObj.transform.SetParent(this.transform, false);
+            RectTransform rectTransform = buttonObj.AddComponent<RectTransform>();
+            rectTransform.sizeDelta = new Vector2(160, 30);
+            rectTransform.anchoredPosition = new Vector3(0, i * 50);
+
+            Button button = buttonObj.AddComponent<Button>();
+            Image image = buttonObj.AddComponent<Image>();
+
+            buttonObj.transform.SetParent(canvas.transform, false);
+            GameObject textObj = new("ButtonText");
+            textObj.transform.SetParent(buttonObj.transform, false);
+
+            // button init
+            button.targetGraphic = image;
+            // text init
+            Text text = textObj.AddComponent<Text>();
+            text.text = "Click Me!";
+            text.font = Resources.GetBuiltinResource<Font>("LegacyRuntime.ttf");
+            text.color = Color.black;
+            text.alignment = TextAnchor.MiddleCenter;
+
+            // image.sprite = Resources.Load<Sprite>("Resources/unity_builtin_extra/Background");
+
+            RectTransform textRectTransform = textObj.GetComponent<RectTransform>();
+            textRectTransform.sizeDelta = new Vector2(160, 30);
+            button.onClick.AddListener(() => ButtonClicked());
+        }
     }
 
     void ButtonClicked()
